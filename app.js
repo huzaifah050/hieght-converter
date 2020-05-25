8b9ce10c00a0f9c29d2202b27973feb12da0ab3f
Huzaifah Attah <hhuzaifah050@gmail.com>
2020-05-25 14:24:48 +0000

    all files

---
+++ app.js
@@ -0,0 +1,37 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  document.getElementById('output').style.visibility = 'visible';
+
+  const feet = document.getElementById('feet').value;
+  const inches = document.getElementById('inches').value;
+
+  if (!feet && !inches) {
+    alert('Please fill the form');
+    return false;
+  }
+
+  const feetCenti = feet * 30.48;
+  const inchesCenti = inches * 2.84;
+  const total = feetCenti + inchesCenti;
+
+  output = document.querySelector('.output');
+  output.innerHTML =
+    '<h3 class = "total">' +
+    total +
+    '<span>' +
+    'centimetres' +
+    '</span>' +
+    '</h3>';
+
+  document.getElementById('form').reset();
+});
+
+
+
+
+
+
+
